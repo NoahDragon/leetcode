b9b029fc60a18589fc52a585f44581762f29a770
Abner Chou <zhcjtht@hotmail.com>
2017-07-07 16:18:30 -0400

    add comments

--- 50.powx-n.cpp
+++ 50.powx-n.cpp
@@ -12,10 +12,10 @@ public:
 	if ( n == 0)
 		return 1;
 
-	if ( x == 1.0)
+	if ( x == 1.0) // when x = 1, no matter what n is, the result is 1.
 		return 1.0;
 
-	if ( x == -1.0){
+	if ( x == -1.0){ // when x = -1, depends on n, if n is odd number, then -1, if n is even number, then 1.
 		if(n%2 == 0)
 			return 1;
 		else
@@ -25,7 +25,7 @@ public:
 	if ( n > 0 ){
     		for(int i = 0; i < n-1; i++){
 			y *= x;
-			if ( y == 0.0)
+			if ( y == 0.0) // to avoid exceed the time limit. 0.00001^25678983
 				break;
 		}
 	}
@@ -34,7 +34,7 @@ public:
 		n = -n;
 		for (int i = 0; i < n-1; i++){
 			y *= x;
-			if ( 1.0/y == 0.0)
+			if ( 1.0/y == 0.0) // to avoid exceed the time limit. 0.00001^-25678983
 				break;
 		}
 		y = 1.0/y;
