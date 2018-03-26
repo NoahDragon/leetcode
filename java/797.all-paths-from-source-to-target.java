24437dc7992007df00b284d35e7168fed8b4e117
Abner Chou <zhcjtht@hotmail.com>
2018-03-26 18:01:51 -0400

    Add new problems

---
+++ java/797.all-paths-from-source-to-target.java
@@ -0,0 +1,43 @@
+/*
+ * [813] All Paths From Source to Target
+ *
+ * https://leetcode.com/problems/all-paths-from-source-to-target/description/
+ *
+ * algorithms
+ * Medium (68.70%)
+ * Total Accepted:    4.1K
+ * Total Submissions: 5.9K
+ * Testcase Example:  '[[1,2],[3],[3],[]]'
+ *
+ * Given a directed, acyclic graph of N nodes.  Find all possible paths from
+ * node 0 to node N-1, and return them in any order.
+ * 
+ * The graph is given as follows:  the nodes are 0, 1, ..., graph.length - 1.
+ * graph[i] is a list of all nodes j for which the edge (i, j) exists.
+ * 
+ * 
+ * Example:
+ * Input: [[1,2], [3], [3], []] 
+ * Output: [[0,1,3],[0,2,3]] 
+ * Explanation: The graph looks like this:
+ * 0--->1
+ * |    |
+ * v    v
+ * 2--->3
+ * There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * The number of nodes in the graph will be in the range [2, 15].
+ * You can print different paths in any order, but you should keep the order of
+ * nodes inside one path.
+ * 
+ * 
+ */
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        
+    }
+}
